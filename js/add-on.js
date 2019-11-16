c8c3cbd04ab4e39bdb889dfcd7633753faf83bec
ishiDACo <2542762+ishiDACo@users.noreply.github.com>
2019-11-16 16:11:32 +0900

    Improved WebFont sessionStorage caching.

--- js/add-on.js
+++ js/add-on.js
@@ -12,8 +12,7 @@ WebFontConfig = {
 };
 
 (function (d) {
-  var wf = d.createElement('script'), s = d.scripts[0];
-  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-  wf.async = 'true';
-  s.parentNode.insertBefore(wf, s);
+  if (sessionStorage.fonts) {
+    d.documentElement.classList.add('wf-active');
+  }
 })(document);
