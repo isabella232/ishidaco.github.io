49f962784ca56fbe23848a580396626fef008636
ishiDACo <2542762+ishiDACo@users.noreply.github.com>
2019-11-16 15:39:07 +0900

    Changed Web Font loading method. Changed font-family from M PLUS 1p to Noto Sans JP.

--- js/add-on.js
+++ js/add-on.js
@@ -4,3 +4,16 @@
   is an official part of the theme (ex. Pull Requests) should be included in main.js
   and follow the formatting and style given.
 */
+WebFontConfig = {
+  google: { families: ['Ubuntu:400,700', 'Source+Sans+Pro:400,700', 'Noto+Sans+JP:400,700'] },
+  active: function () {
+    sessionStorage.fonts = true;
+  }
+};
+
+(function (d) {
+  var wf = d.createElement('script'), s = d.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+  wf.async = 'true';
+  s.parentNode.insertBefore(wf, s);
+})(document);
